3f2348ff02826c1109a3ced110c54ccc06c9fd96
uncleSanya <22061999@inbox.ru>
2017-07-18 22:59:23 +0300

    list&foreach

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -16,6 +16,25 @@ namespace snake
             p_Draw p2 = new p_Draw(1, 5, '@');
             p2.Draw();
 
+            List < int > kekaem = new List<int>();
+            kekaem.Add(1);
+            kekaem.Add(4);
+            kekaem.Add(7);
+
+            foreach (int i in kekaem)
+            {
+                Console.WriteLine(i);
+            }
+
+            List<p_Draw> kakaem = new List<p_Draw>();
+            kakaem.Add(p1);
+            kakaem.Add(p2);
+
+            foreach (p_Draw i in kakaem)
+            {
+                i.Draw();
+            }
+
             Console.ReadKey();
         }
     }
