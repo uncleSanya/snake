82e79a2485656c2d1310fc1a5141e864e3f09a7a
uncleSanya <22061999@inbox.ru>
2017-07-18 23:28:04 +0300

    горизонтальная линия

---
+++ snake/snake/HorLine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class HorLine
+    {
+        List<p_Draw> pline;
+
+        public HorLine()
+        {
+            pline = new List<p_Draw>();
+            p_Draw p1 = new p_Draw(5, 5, '*');
+            p_Draw p2 = new p_Draw(6, 5, '*');
+            p_Draw p3 = new p_Draw(7, 5, '*');
+            pline.Add(p1);
+            pline.Add(p2);
+            pline.Add(p3);
+        }
+        public void Draw()
+        {
+            foreach (p_Draw i in pline)
+            {
+                i.Draw();
+            }
+        }
+    }
+}
--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -16,24 +16,8 @@ namespace snake
             p_Draw p2 = new p_Draw(1, 5, '@');
             p2.Draw();
 
-            List < int > kekaem = new List<int>();
-            kekaem.Add(1);
-            kekaem.Add(4);
-            kekaem.Add(7);
-
-            foreach (int i in kekaem)
-            {
-                Console.WriteLine(i);
-            }
-
-            List<p_Draw> kakaem = new List<p_Draw>();
-            kakaem.Add(p1);
-            kakaem.Add(p2);
-
-            foreach (p_Draw i in kakaem)
-            {
-                i.Draw();
-            }
+            HorLine line = new HorLine();
+            line.Draw();
 
             Console.ReadKey();
         }
