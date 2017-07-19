42534a359faf4fa0ff3cc7dc29ebed0ccdf7fcbf
uncleSanya <22061999@inbox.ru>
2017-07-19 11:38:57 +0300

    kvadrat

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,17 +10,23 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(4,9,'$');
+            /*Point p1 = new Point(4,9,'$');
             p1.Draw();
 
             Point p2 = new Point(1, 5, '@');
-            p2.Draw();
+            p2.Draw();*/
 
-            HorLine line = new HorLine(10,30,5,'&');
+            HorLine line = new HorLine(10,50,5,'-');
             line.Draw();
 
-            VertLine newline = new VertLine(20, 5, 15, '&');
+            VertLine newline = new VertLine(10, 5, 25, '|');
             newline.Draw();
+
+            HorLine line1 = new HorLine(10, 50, 25, '-');
+            line1.Draw();
+
+            VertLine newline1 = new VertLine(50, 5, 25, '|');
+            newline1.Draw();
 
             Console.ReadKey();
         }
