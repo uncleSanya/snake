69f46f0b6efed540b89c99bae98728be0d51cba4
uncleSanya <22061999@inbox.ru>
2017-07-19 12:33:43 +0300

    sn+napr

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -13,17 +13,24 @@ namespace snake
             Console.SetWindowSize(80,25);
             Console.SetBufferSize(80,25);
 
-            HorLine upline = new HorLine(0,78,0,'*');
+            HorLine upline = new HorLine(0,78,0,'+');
             upline.Draw();
 
-            VertLine leftline = new VertLine(0, 0, 24, '*');
+            VertLine leftline = new VertLine(0, 0, 24, '+');
             leftline.Draw();
 
-            HorLine downline = new HorLine(0, 78, 24, '*');
+            HorLine downline = new HorLine(0, 78, 24, '+');
             downline.Draw();
 
-            VertLine rightline = new VertLine(78, 0, 24, '*');
+            VertLine rightline = new VertLine(78, 0, 24, '+');
             rightline.Draw();
+
+
+            // Змейка
+
+            Point p = new Point(10,10,'*');
+            Snake zhora = new Snake(p, 5, Napr.DOWN);
+            zhora.Draw();
 
             Console.ReadKey();
         }
---
+++ snake/snake/Snake.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class Snake : Figure
+    {
+        public Snake(Point start, int lenght, Napr napr)
+        {
+
+        }
+    }
+}
